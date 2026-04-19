22c66bdfa5c547ac6ad174056f3f4162de366491
IslesPJ <isles.pj@gmail.com>
2026-04-19 20:26:06 +0900

    Fixed a gitignore file.

---
+++ config.py
@@ -0,0 +1,12 @@
+smtp_server = "smtp.SERVER.com"
+smtp_port = 465
+imap_server = "imap.SERVER.com"
+imap_port = 993
+email_user = "sender_email@email.com"
+email_password = ""
+
+receiver_email = "receiver@email.com"
+email_subject = "Screen Changed!"
+
+tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+output_img_path = "./output.png"
